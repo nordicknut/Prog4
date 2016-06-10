d27c911e4a635405fa20086307903b8a533ec71c
jpb <jpbrastad@hotmail.com>
2016-06-09 17:00:41 -0700

    Project setup

---
+++ Prog4/Prog4.cpp
@@ -0,0 +1,14 @@
+// Prog4.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Project setup." << endl;
+    return 0;
+}
+
