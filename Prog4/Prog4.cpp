de756ed93f7750b1794a733ec6e3ff657c59000c
jpb <jpbrastad@hotmail.com>
2016-06-09 17:14:43 -0700

    Updated with source.

--- Prog4/Prog4.cpp
+++ Prog4/Prog4.cpp
@@ -1,14 +1,31 @@
 // Prog4.cpp : Defines the entry point for the console application.
 //
 
-#include <iostream>
-
+#include "BST.h"
 using namespace std;
-
-
-int main()
+// CS212, Spring 2014, Program #5
+void main()
 {
-	cout << "Project setup." << endl;
-    return 0;
-}
+	// Create a binary search tree for strings
+	BST<string> tree;
 
+	// Add elements to the tree
+	tree.insert("America");
+	tree.insert("Canada");
+	tree.insert("Russia");
+	tree.insert("France");
+
+	// Create an Iterator
+	Iterator<string> iterator = tree.begin();
+
+	tree.insert("Texas"); // modify the tree
+
+						  // Traverse a binary tree using iterators
+	//while (iterator != tree.end())
+	//{
+	//	cout << *iterator << " ";
+	//	iterator++;
+	//}
+	// Texas is missing, FIX IT!!!!
+	system("pause");
+}
